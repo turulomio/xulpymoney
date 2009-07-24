6f5ce16ac5e8d98751323d29076c874ee057cfb5
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2009-07-24 09:59:24 +0000

    Añadido soporte para tres decimales en los valores de las acciones

--- clases/formato.py
+++ clases/formato.py
@@ -1,12 +1,15 @@
-# -*- coding: UTF-8  -*-
+# -*- coding: UTF-8 -*-
 import string
 
 def dosdecimales(num):
     return float("%.2f" % num)
     
-def euros(num):
-    return "%.2f €" %num
-    
+def euros(num,  numdec=2):
+    if numdec==2:
+        return "%.2f €" %num
+    elif numdec==3:
+        return "%.3f €" %num
+
 def tpc(num):
     str="%.2f " %num
     return str+"%"
