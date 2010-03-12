061160745f7914172a984df886d21555d2bd00f0
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2010-03-12 05:07:33 +0000

    Añadida funcionalidad de diff

--- scripts/histórico_ibex35.py
+++ scripts/histórico_ibex35.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python    
 # -*- coding: utf-8 -*-  
-import os, sys, adodb, datetime
+import os, adodb, datetime
 
 cwd=os.getcwd()
 
@@ -12,15 +12,24 @@ con.Connect("127.0.0.1","postgres","*","cuentas")
 con.Execute("delete from ibex35")
 
 # SE DESCARGA UNA NUEVA
-hoy=datetime.date.today()
-os.system ("wget 'http://ichart.yahoo.com/table.csv?s=%5EIBEX&a=01&b=15&c=1993&d=" + str(datetime.date.today().month-1) + "&e=" + str(datetime.date.today().day) + "&f="+ str(datetime.date.today().year) + "&g=d&ignore=.csv' -O /tmp/ibex35.csv")
+print "* Descargando fichero..."
+os.system ("wget 'http://ichart.yahoo.com/table.csv?s=%5EIBEX&a=01&b=15&c=1993&d=" + str(datetime.date.today().month-1) + "&e=" + str(datetime.date.today().day) + "&f="+ str(datetime.date.today().year) + "&g=d&ignore=.csv' -O /tmp/ibex35.csv &> /dev/null")
 
 # INSERTA LA NUEVA
+print "* Procesando el fichero..."
 f=open("/tmp/ibex35.csv","r")
 f.readline()
+arr=[]
 for line in f:
 	arrline=line.split(",")
-	con.Execute ("insert into ibex35 (fecha, cierre) values ('" + arrline[0] + "', " +  arrline[4] + ");")
+	arr.append((arrline[0], float(arrline[4]), 0.0))
 f.close()
-con.Close()
 
+
+n=len(arr)-1
+con.Execute ("insert into ibex35 (fecha, cierre, diff) values ('" + arr[n][0] + "', " + str(arr[n][1]) + ", "+ str(arr[n][2])+ ");")
+for i in range(0,n):
+	arr[n-1-i]=(arr[n-1-i][0], arr[n-1-i][1], arr[n-1-i][1]-arr[n-i][1])
+        con.Execute ("insert into ibex35 (fecha, cierre, diff) values ('" + arr[n-1-i][0] + "', " + str(arr[n-1-i][1]) + ", "+ str(arr[n-1-i][2])+ ");")
+con.Close()
+print "  - Se han añadido", len (arr), "registros"
