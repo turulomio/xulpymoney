d70634c0b0263cddf1af6f54615b111527b0814f
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2010-05-03 11:34:03 +0000

    Solucionado imagan de informe total ahora con svg

--- clases/formato.py
+++ clases/formato.py
@@ -10,6 +10,16 @@ def euros(num,  numdec=2):
     elif numdec==3:
         return "%.3f €" %num
 
+def svg2string(file):
+    f=open(file, "r")
+    f.readline()
+    f.readline()
+    f.readline()
+    f.readline()
+    s='<svg flex="1" style="width:100%; height:100%;" viewBox="0 0 1280 1024"\n'+f.read()
+    f.close()
+    return s
+    
 def tpc(num):
     str="%.2f " %num
     return str+"%"
