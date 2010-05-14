bcdc7ac83448b53a39adb734562bf4691ee65f43
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2010-05-14 22:43:15 +0000

    Se ha añadido soporte para actualizar la inversión con doble click

--- clases/formato.py
+++ clases/formato.py
@@ -9,6 +9,10 @@ def euros(num,  numdec=2):
         return "%.2f €" %num
     elif numdec==3:
         return "%.3f €" %num
+    elif numdec==4:
+        return "%.4f €" %num
+    elif numdec==5:
+        return "%.5f €" %num
     
 def tpc(num):
     str="%.2f " %num
--- clases/inversion_actualizacion.py
+++ clases/inversion_actualizacion.py
@@ -2,8 +2,25 @@
 import urllib
 
 def comaporpunto(cadena):
-    return cadena.replace(',','.')
-    
+    cadena=cadena.replace('.','')#Quita puntos
+    cadena=cadena.replace(',','.')#Cambia coma por punto
+    return cadena
+
+def bankintergestion(arr):
+    try:
+        web=urllib.urlopen('http://www.bolsamadrid.es/esp/mercados/fondos/htmfondos/0055alfa.htm')
+    except:
+        return arr
+    lineainv=""
+    for i in range(30):
+        web.readline()
+    for line in web.readlines():    
+        if line.find("fonficha") != -1:
+            nombre=line.split(')">')[1].split("</a></TD>")[0]
+            valor=float(comaporpunto(line.split("</TD><TD>")[1]))
+            arr.append((nombre, valor))
+    return arr
+
 def bolsamadrid(arr):
     try:
         web=urllib.urlopen('http://www.bolsamadrid.es/esp/mercados/acciones/accmerc2_c.htm')
@@ -18,7 +35,34 @@ def bolsamadrid(arr):
             valor=float(comaporpunto(line.split("</A></TD><TD>")[1].split("</TD>")[0]))
             arr.append((nombre, valor))
     return arr
-    
+
+def carmignacpatrimoinea(arr):
+    try:
+        web=urllib.urlopen('http://www.carmignac.es/es/carmignac-patrimoine-part-a.htm')
+    except:
+        return arr
+    lineainv=""
+    for line in web.readlines():
+        if line.find('<?xml version="1.0" encoding="utf-8"?><p class="dr"><span>') != -1:
+            nombre="CARMIGNAC PATRIMOINE A"
+            valor=float(line.split("<span>")[1].split(" ")[0])
+            arr.append((nombre, valor))
+    return arr
+
+def LyxorETFXBearEUROSTOXX50(arr):
+    try:
+        web=urllib.urlopen('http://www.boerse-frankfurt.de/EN/index.aspx?pageID=105&ISIN=FR0010424143')
+    except:
+        return arr
+    lineainv=""
+    for line in web.readlines():
+        if line.find('Last Price') != -1:
+            nombre="LyxorETFXBearEUROSTOXX50"
+            valor=float(line.split("<span><b>")[2].split("</b>")[0])
+            arr.append((nombre, valor))
+    return arr
+
+
 def getvalor(arr, valor):
     for i in arr:
         if i[0]==valor:
