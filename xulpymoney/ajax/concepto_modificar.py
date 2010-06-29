6b339aa57a6456b1388b61c48909d874d695b9e3
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2010-06-29 02:37:46 +0000

    Solucionado pequeños bugs y añadidos dos scripts

--- xulpymoney/ajax/concepto_modificar.py
+++ xulpymoney/ajax/concepto_modificar.py
@@ -1,7 +1,7 @@
 #-*- coding: UTF-8 -*-
-from mod_python import util
 from core import *
 def index(req):
+    form=req.form
     con=Conection()
     if Concepto().modificar(form['id_conceptos'], form['concepto'],  form['id_tiposoperaciones'])==True:
         req.write ("Se ha modificado el dato")
