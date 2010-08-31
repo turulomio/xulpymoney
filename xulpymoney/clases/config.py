064c0ffc5ba05a14c942f27b71f257594e58be5b
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2010-08-31 18:27:36 +0000

    Mejorando scripts

--- xulpymoney/clases/config.py
+++ xulpymoney/clases/config.py
@@ -1,9 +1,11 @@
 # -*- coding: UTF-8  -*-
-host='localhost'
+host='127.0.0.1'
 dbname='xulpymoney'
 user='postgres'
 password='xxx'
+port=5432
 type='postgres'
 language='es'# es|en
+
 dividendwithholding=0.18
 taxcapitalappreciation=0.18
