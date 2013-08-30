93a7de267075e2a5d8a41c078c3f333af89f19e9
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2013-08-30 04:27:18 +0000

    Solved bug con .yahoo

--- xulpymoney/sources/mq.yahoohistorical.py
+++ xulpymoney/sources/mq.yahoohistorical.py
@@ -33,7 +33,7 @@ class WorkerYahooHistorical(Source):
         
     def execute(self,  investment, inicio, fin):
         """inico y fin son dos dates entre los que conseguir los datos."""
-        url='http://ichart.finance.yahoo.com/table.csv?s='+investment.yahoo+'&a='+str(inicio.month-1)+'&b='+str(inicio.day)+'&c='+str(inicio.year)+'&d='+str(fin.month-1)+'&e='+str(fin.day)+'&f='+str(fin.year)+'&g=d&ignore=.csv'
+        url='http://ichart.finance.yahoo.com/table.csv?s='+investment.ticker+'&a='+str(inicio.month-1)+'&b='+str(inicio.day)+'&c='+str(inicio.year)+'&d='+str(fin.month-1)+'&e='+str(fin.day)+'&f='+str(fin.year)+'&g=d&ignore=.csv'
         (set, error)=(SetQuotes(self.cfg), [])
         web=self.download(url, 'YAHOO_HISTORICAL')
         if web==None:
