1f6a787e0eeb031e7570f797451d96ee4980e771
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2014-12-14 07:36:54 +0000

    wdgJoint now calculates gains below a year correctly. Asked in ticket #17

--- ui/wdgJointReport.py
+++ ui/wdgJointReport.py
@@ -45,10 +45,12 @@ class wdgJointReport(QWidget, Ui_wdgJointReport):
         if str2bool(self.mem.config.get_value("settings", "gainsyear"))==True:
             self.load_less()
             self.load_more()
+            self.load_added()
+            self.load_rendimientos_less()
         else:
             self.load_historicas()
-        self.load_added()
-        self.load_rendimientos()
+            self.load_added()
+            self.load_rendimientos()
         print("wdgJointReport > load: {0}".format(datetime.datetime.now()-inicio))
         
     def load_added(self):
@@ -91,6 +93,7 @@ class wdgJointReport(QWidget, Ui_wdgJointReport):
                     operaciones.arr.append(o)
         operaciones.sort()
         (self.totalBruto, self.totalComisiones, self.totalImpuestos, self.totalNeto)=operaciones.myqtablewidget(self.tblInvestments, "wdgJointReport")
+
     def load_less(self):
         operaciones=SetInvestmentOperationsHistorical(self.mem)
         for i in self.mem.data.investments_all().arr:
@@ -98,7 +101,8 @@ class wdgJointReport(QWidget, Ui_wdgJointReport):
                 if o.fecha_venta.year==self.wy.year and o.tipooperacion.id in (5, 8) and o.less_than_a_year()==True:#Venta y traspaso fondos inversion
                     operaciones.arr.append(o)
         operaciones.sort()
-        (self.totalBruto, self.totalComisiones, self.totalImpuestos, self.totalNeto)=operaciones.myqtablewidget(self.tblLess, "wdgJointReport")
+        (self.totalBrutoLess, self.totalComisionesLess, self.totalImpuestosLess, self.totalNetoLess)=operaciones.myqtablewidget(self.tblLess, "wdgJointReport")
+
     def load_more(self):
         operaciones=SetInvestmentOperationsHistorical(self.mem)
         for i in self.mem.data.investments_all().arr:
@@ -106,11 +110,107 @@ class wdgJointReport(QWidget, Ui_wdgJointReport):
                 if o.fecha_venta.year==self.wy.year and o.tipooperacion.id in (5, 8) and o.less_than_a_year()==False:#Venta y traspaso fondos inversion
                     operaciones.arr.append(o)
         operaciones.sort()
-        (self.totalBruto, self.totalComisiones, self.totalImpuestos, self.totalNeto)=operaciones.myqtablewidget(self.tblMore, "wdgJointReport")
+        (self.totalBrutoMore, self.totalComisionesMore, self.totalImpuestosMore, self.totalNetoMore)=operaciones.myqtablewidget(self.tblMore, "wdgJointReport")
 
 
+    def load_rendimientos_less(self):
+        """Rendimientos cuando contabilizan distinto las ganancias a menos de un año"""
+        inicio=datetime.date(self.wy.year-1, 12, 31)
+        cur=self.mem.con.cursor()
+        cur.execute("select sum(importe) as suma from opercuentas where id_conceptos=59 and date_part('year',datetime)="+str(self.wy.year))
+        sumcomisioncustodia=cur.fetchone()[0]        
+        if sumcomisioncustodia==None:
+            sumcomisioncustodia=0
+            
+        saldototal=Assets(self.mem).saldo_total(self.mem.data.investments_all() ,  datetime.date.today());
+        saldototalinicio=Assets(self.mem).saldo_total( self.mem.data.investments_all(), inicio)
+        
+        self.totalBruto=self.totalBrutoLess+self.totalBrutoMore
+        self.totalComisiones=self.totalComisionesLess+self.totalComisionesMore
+        self.totalImpuestos=self.totalImpuestosLess+self.totalImpuestosMore
+        self.totalNeto=self.totalNetoLess+self.totalNetoMore
+        
+        if self.totalBruto>0:
+            impxplus=-self.totalBrutoLess*self.mem.taxcapitalappreciationbelow-self.totalBrutoMore*self.mem.taxcapitalappreciation
+        else:            
+            impxplus=0
+        
+        beneficiosin=self.totalBruto+self.totalComisiones+sumcomisioncustodia+self.totalDividendsBrutos
+        beneficiopag=self.totalBruto+impxplus+self.totalImpuestos+self.totalComisiones+sumcomisioncustodia+self.totalDividendsNetos
+        
+        if saldototalinicio==0:
+            tpcneto=None
+            tpcimpxplus=None
+            tpcplus=None
+            tpcdividendsbrutos=None
+            tpcdividendsnetos=None
+            tpcsumcomision=None
+            tpcsumcustodia=None
+            tpcsumimpuestos=None
+            tpcretenciondividends=None
+        else:
+            tpcneto=self.totalNeto*100/saldototalinicio
+            tpcimpxplus=impxplus*100/saldototalinicio
+            tpcplus=self.totalBruto*100/saldototalinicio
+            tpcdividendsbrutos=self.totalDividendsBrutos*100/saldototalinicio
+            tpcdividendsnetos=self.totalDividendsNetos*100/saldototalinicio
+            tpcsumcomision=self.totalComisiones*100/saldototalinicio
+            tpcsumcustodia=sumcomisioncustodia*100/saldototalinicio
+            tpcsumimpuestos=self.totalImpuestos*100/saldototalinicio
+            tpcretenciondividends=self.totalDividendsRetenciones*100/saldototalinicio
+
+        self.tblEstudio.horizontalHeaderItem(1).setText(self.tr("% APR from {}").format(inicio))
+        self.lblSaldo.setText(self.tr("Balance at {0}, {1}".format(str(inicio), self.mem.localcurrency.string(saldototalinicio))))
+
+        self.tblEstudio.setItem(0, 0,self.mem.localcurrency.qtablewidgetitem(self.totalBruto))       
+        self.tblEstudio.setItem(0, 1,qtpc(tpcplus))            
+
+        self.tblEstudio.setItem(1, 0,self.mem.localcurrency.qtablewidgetitem(self.totalComisiones))       
+        self.tblEstudio.setItem(1, 1,qtpc(tpcsumcomision))     
+
+        self.tblEstudio.setItem(2, 0,self.mem.localcurrency.qtablewidgetitem(self.totalImpuestos))       
+        self.tblEstudio.setItem(2, 1,qtpc(tpcsumimpuestos))    
+        
+        self.tblEstudio.setItem(3, 0,self.mem.localcurrency.qtablewidgetitem(self.totalNeto))    #Lo que sale en total y patrimonio   
+        self.tblEstudio.setItem(3, 1,qtpc(tpcneto))      
+
+        self.tblEstudio.setItem(5, 0,self.mem.localcurrency.qtablewidgetitem(self.totalDividendsBrutos))       
+        self.tblEstudio.setItem(5, 1,qtpc(tpcdividendsbrutos))     
+        
+        self.tblEstudio.setItem(6, 0,self.mem.localcurrency.qtablewidgetitem(self.totalDividendsRetenciones))       
+        self.tblEstudio.setItem(6, 1,qtpc(tpcretenciondividends))            
+
+        self.tblEstudio.setItem(7, 0,self.mem.localcurrency.qtablewidgetitem(self.totalDividendsNetos))       
+        self.tblEstudio.setItem(7, 1,qtpc(tpcdividendsnetos))                
+        
+        self.tblEstudio.setItem(9, 0,self.mem.localcurrency.qtablewidgetitem(sumcomisioncustodia))       
+        self.tblEstudio.setItem(9, 1,qtpc(tpcsumcustodia))      
+
+        self.tblEstudio.setItem(11, 0,self.mem.localcurrency.qtablewidgetitem(impxplus))     
+        self.tblEstudio.setItem(11, 1, qtpc(tpcimpxplus))
+
+        self.tblEstudio.setItem(13, 0,self.mem.localcurrency.qtablewidgetitem(beneficiosin))       
+        try:
+            self.tblEstudio.setItem(13, 1,qtpc((beneficiosin)*100/saldototalinicio))            
+        except:
+            self.tblEstudio.setItem(13, 1,qtpc(None))     
+
+        self.tblEstudio.setItem(14, 0,self.mem.localcurrency.qtablewidgetitem(beneficiopag))       
+        try:
+            self.tblEstudio.setItem(14, 1,qtpc((beneficiopag)*100/saldototalinicio))            
+        except:
+            self.tblEstudio.setItem(14,  1,qtpc(None))     
+
+        self.tblEstudio.setItem(16, 0,self.mem.localcurrency.qtablewidgetitem(saldototal))       
+        try:
+            self.tblEstudio.setItem(16, 1,qtpc((saldototal-saldototalinicio)*100/saldototalinicio))            
+        except:
+            self.tblEstudio.setItem(16, 1,qtpc(None))     
+            
+        cur.close()
 
     def load_rendimientos(self):
+        """Rendimientos cuando  no contabilizan distinto las ganancias  de un año"""
         inicio=datetime.date(self.wy.year-1, 12, 31)
         cur=self.mem.con.cursor()
         cur.execute("select sum(importe) as suma from opercuentas where id_conceptos=59 and date_part('year',datetime)="+str(self.wy.year))
