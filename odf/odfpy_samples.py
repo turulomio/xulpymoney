9084f167f583e4dd73f8b3af35675f7e3604d707
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2015-04-02 17:41:26 +0000

    Function eschema. Added metadata

--- odf/odfpy_samples.py
+++ odf/odfpy_samples.py
@@ -1,6 +1,6 @@
-
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
+
 
 
 ## ODT generated directly
@@ -9,107 +9,121 @@ import odf.style
 import odf.text
 import odf.table
 import odf.draw
-#from odf.opendocument import OpenDocumentText
-#from odf.style import Style, TextProperties
-#from odf.text import H, P, Span
+import odf.meta
+import odf.dc
 
-textdoc = odf.opendocument.OpenDocumentText()
-# Styles
-s = textdoc.styles
-h1style = odf.style.Style(name="Heading 1", family="paragraph")
-h1style.addElement(odf.style.TextProperties(attributes={'fontsize':"24pt",'fontweight':"bold" }))
-s.addElement(h1style)
+def odt_generated_directly():
+    def metadata():
+        textdoc.meta.addElement(odf.dc.Title(text="pyodf example"))
+        textdoc.meta.addElement(odf.dc.Description(text=u"This is a nice description of Muñoz"))
+        textdoc.meta.addElement(odf.meta.InitialCreator(text=u"Mariano Muñoz Marqu´inez"))
+        textdoc.meta.addElement(odf.dc.Creator(text=u'đđðæßðđæ€ł'))
+        
+        
+    #######################################
+    textdoc = odf.opendocument.OpenDocumentText()
 
-tablecontents = odf.style.Style(name="Table Contents", family="paragraph")
-tablecontents.addElement(odf.style.ParagraphProperties(numberlines="false", linenumber="0"))
-textdoc.styles.addElement(tablecontents)
+    metadata()
+    
+    
+    
+    # Styles
+    s = textdoc.styles
+    h1style = odf.style.Style(name="Heading 1", family="paragraph")
+    h1style.addElement(odf.style.TextProperties(attributes={'fontsize':"24pt",'fontweight':"bold" }))
+    s.addElement(h1style)
+    
+    tablecontents = odf.style.Style(name="Table Contents", family="paragraph")
+    tablecontents.addElement(odf.style.ParagraphProperties(numberlines="false", linenumber="0"))
+    textdoc.styles.addElement(tablecontents)
+    
+    # Create automatic styles for the column widths.
+    # We want two different widths, one in inches, the other one in metric.
+    # ODF Standard section 15.9.1
+    widthshort = odf.style.Style(name="Wshort", family="table-column")
+    widthshort.addElement(odf.style.TableColumnProperties(columnwidth="1.7cm"))
+    textdoc.automaticstyles.addElement(widthshort)
+    
+    widthwide = odf.style.Style(name="Wwide", family="table-column")
+    widthwide.addElement(odf.style.TableColumnProperties(columnwidth="1.5in"))
+    textdoc.automaticstyles.addElement(widthwide)
+    
+    # An automatic style
+    boldstyle = odf.style.Style(name="Bold", family="text")
+    boldprop = odf.style.TextProperties(fontweight="bold")
+    boldstyle.addElement(boldprop)
+    textdoc.automaticstyles.addElement(boldstyle)
+    
+    
+    
+    
+    # Text
+    h=odf.text.H(outlinelevel=1, stylename=h1style, text="My first text")
+    textdoc.text.addElement(h)
+    p = odf.text.P(text="Hello world. ")
+    boldpart = odf.text.Span(stylename="Bold",text="This part is bold. ")
+    p.addElement(boldpart)
+    p.addText("This is after bold.")
+    textdoc.text.addElement(p)
+    
+    table = odf.table.Table()
+    table.addElement(odf.table.TableColumn(numbercolumnsrepeated=4,stylename=widthshort))
+    table.addElement(odf.table.TableColumn(numbercolumnsrepeated=3,stylename=widthwide))
+    
+    f = open('/etc/passwd')
+    for i in range(5):
+    #for line in f:
+        line=f.readline()
+        rec = line.strip().split(":")
+        tr = odf.table.TableRow()
+        table.addElement(tr)
+        for val in rec:
+            tc = odf.table.TableCell()
+            tr.addElement(tc)
+            p = odf.text.P(stylename=tablecontents,text=val)
+            tc.addElement(p)
+    
+    textdoc.text.addElement(table)
+    
+    
+    ###Generating images
+    textdoc.text.addElement(odf.text.H(outlinelevel=1,text='Generating An Image', stylename=h1style))
+    p = odf.text.P()
+    
+    href = textdoc.addPicture("../images/spain.gif")
+    f = odf.draw.Frame(name="graphics1", anchortype="character") #, width="2cm", height="2cm", zindex="0")
+    p.addElement(f)
+    img = odf.draw.Image(href=href, type="simple", show="embed", actuate="onLoad")
+    f.addElement(img)
+    textdoc.text.addElement(p)
+    
+    
+    
+    ###Foot note
+    textdoc.text.addElement(odf.text.H(outlinelevel=1,text='Footnotes (Heading 1)',stylename=h1style))
+    p = odf.text.P()
+    textdoc.text.addElement(p)
+    p.addText("This sentence has an accompanying footnote.")
+    note = odf.text.Note(id="ftn0", noteclass="footnote")
+    p.addElement(note)
+    note.addElement(odf.text.NoteCitation(text='1'))
+    notebody = odf.text.NoteBody()
+    note.addElement(notebody)
+    notebody.addElement(odf.text.P(stylename="Footnote", text="You are reading a footnote."))
+    p.addElement(odf.text.S(c=2))
+    p.addText("Where does the text after a footnote go?")
+    
+    
+    
+    
+    textdoc.save("odfpy_generated_directly.odt")
 
-# Create automatic styles for the column widths.
-# We want two different widths, one in inches, the other one in metric.
-# ODF Standard section 15.9.1
-widthshort = odf.style.Style(name="Wshort", family="table-column")
-widthshort.addElement(odf.style.TableColumnProperties(columnwidth="1.7cm"))
-textdoc.automaticstyles.addElement(widthshort)
-
-widthwide = odf.style.Style(name="Wwide", family="table-column")
-widthwide.addElement(odf.style.TableColumnProperties(columnwidth="1.5in"))
-textdoc.automaticstyles.addElement(widthwide)
-
-# An automatic style
-boldstyle = odf.style.Style(name="Bold", family="text")
-boldprop = odf.style.TextProperties(fontweight="bold")
-boldstyle.addElement(boldprop)
-textdoc.automaticstyles.addElement(boldstyle)
 
 
 
 
-# Text
-h=odf.text.H(outlinelevel=1, stylename=h1style, text="My first text")
-textdoc.text.addElement(h)
-p = odf.text.P(text="Hello world. ")
-boldpart = odf.text.Span(stylename="Bold",text="This part is bold. ")
-p.addElement(boldpart)
-p.addText("This is after bold.")
-textdoc.text.addElement(p)
-
-table = odf.table.Table()
-table.addElement(odf.table.TableColumn(numbercolumnsrepeated=4,stylename=widthshort))
-table.addElement(odf.table.TableColumn(numbercolumnsrepeated=3,stylename=widthwide))
-
-f = open('/etc/passwd')
-for i in range(5):
-#for line in f:
-    line=f.readline()
-    rec = line.strip().split(":")
-    tr = odf.table.TableRow()
-    table.addElement(tr)
-    for val in rec:
-        tc = odf.table.TableCell()
-        tr.addElement(tc)
-        p = odf.text.P(stylename=tablecontents,text=val)
-        tc.addElement(p)
-
-textdoc.text.addElement(table)
-
-
-###Generating images
-textdoc.text.addElement(odf.text.H(outlinelevel=1,text='Generating An Image', stylename=h1style))
-p = odf.text.P()
-
-href = textdoc.addPicture("../images/spain.gif")
-f = odf.draw.Frame(name="graphics1", anchortype="character") #, width="2cm", height="2cm", zindex="0")
-p.addElement(f)
-img = odf.draw.Image(href=href, type="simple", show="embed", actuate="onLoad")
-f.addElement(img)
-textdoc.text.addElement(p)
-
-
-
-###Foot note
-textdoc.text.addElement(odf.text.H(outlinelevel=1,text='Footnotes (Heading 1)',stylename=h1style))
-p = odf.text.P()
-textdoc.text.addElement(p)
-p.addText("This sentence has an accompanying footnote.")
-note = odf.text.Note(id="ftn0", noteclass="footnote")
-p.addElement(note)
-note.addElement(odf.text.NoteCitation(text='1'))
-notebody = odf.text.NoteBody()
-note.addElement(notebody)
-notebody.addElement(odf.text.P(stylename="Footnote", text="You are reading a footnote."))
-p.addElement(odf.text.S(c=2))
-p.addText("Where does the text after a footnote go?")
-
-
-
-
-textdoc.save("odfpy_generated_directly.odt")
-
-
-
-
-
-
+def odt_generated_from_template():
+    pass
 
 
 
@@ -124,43 +138,53 @@ textdoc.save("odfpy_generated_directly.odt")
 
 
 ## ODS generated directly
+def ods_generated_directly():
 
-
-doc = odf.opendocument.OpenDocumentSpreadsheet()
-# Create a style for the table content. One we can modify
-# later in the word processor.
-tablecontents = odf.style.Style(name="Table Contents", family="paragraph")
-tablecontents.addElement(odf.style.ParagraphProperties(numberlines="false", linenumber="0"))
-doc.styles.addElement(tablecontents)
-
-# Create automatic styles for the column widths.
-# We want two different widths, one in inches, the other one in metric.
-# ODF Standard section 15.9.1
-widthshort = odf.style.Style(name="Wshort", family="table-column")
-widthshort.addElement(odf.style.TableColumnProperties(columnwidth="1.7cm"))
-doc.automaticstyles.addElement(widthshort)
-
-widthwide = odf.style.Style(name="Wwide", family="table-column")
-widthwide.addElement(odf.style.TableColumnProperties(columnwidth="1.5in"))
-doc.automaticstyles.addElement(widthwide)
-
-# Start the table, and describe the columns
-table = odf.table.Table(name="Password")
-table.addElement(odf.table.TableColumn(numbercolumnsrepeated=4,stylename=widthshort))
-table.addElement(odf.table.TableColumn(numbercolumnsrepeated=3,stylename=widthwide))
-
-f = open('/etc/passwd')
-for line in f:
-    rec = line.strip().split(":")
-    tr = odf.table.TableRow()
-    table.addElement(tr)
-    for val in rec:
-        tc = odf.table.TableCell()
-        tr.addElement(tc)
-        p = odf.text.P(stylename=tablecontents,text=val)
-        tc.addElement(p)
-
-doc.spreadsheet.addElement(table)
-doc.save("odfpy_generated_directly.ods")
+    doc = odf.opendocument.OpenDocumentSpreadsheet()
+    # Create a style for the table content. One we can modify
+    # later in the word processor.
+    tablecontents = odf.style.Style(name="Table Contents", family="paragraph")
+    tablecontents.addElement(odf.style.ParagraphProperties(numberlines="false", linenumber="0"))
+    doc.styles.addElement(tablecontents)
+    
+    # Create automatic styles for the column widths.
+    # We want two different widths, one in inches, the other one in metric.
+    # ODF Standard section 15.9.1
+    widthshort = odf.style.Style(name="Wshort", family="table-column")
+    widthshort.addElement(odf.style.TableColumnProperties(columnwidth="1.7cm"))
+    doc.automaticstyles.addElement(widthshort)
+    
+    widthwide = odf.style.Style(name="Wwide", family="table-column")
+    widthwide.addElement(odf.style.TableColumnProperties(columnwidth="1.5in"))
+    doc.automaticstyles.addElement(widthwide)
+    
+    # Start the table, and describe the columns
+    table = odf.table.Table(name="Password")
+    table.addElement(odf.table.TableColumn(numbercolumnsrepeated=4,stylename=widthshort))
+    table.addElement(odf.table.TableColumn(numbercolumnsrepeated=3,stylename=widthwide))
+    
+    f = open('/etc/passwd')
+    for line in f:
+        rec = line.strip().split(":")
+        tr = odf.table.TableRow()
+        table.addElement(tr)
+        for val in rec:
+            tc = odf.table.TableCell()
+            tr.addElement(tc)
+            p = odf.text.P(stylename=tablecontents,text=val)
+            tc.addElement(p)
+    
+    doc.spreadsheet.addElement(table)
+    doc.save("odfpy_generated_directly.ods")
 
 ## ODS generated from template
+
+def ods_generated_from_template():
+    pass
+    
+    
+############################################
+odt_generated_directly()
+odt_generated_from_template()
+ods_generated_directly()
+ods_generated_from_template()
