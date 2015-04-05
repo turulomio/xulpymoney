d4c5075b989eabfc4cf6e5e19e364480b23e8cb0
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2015-04-05 07:41:10 +0000

    Added table

--- xulpymoney/libodfgenerator.py
+++ xulpymoney/libodfgenerator.py
@@ -37,106 +37,69 @@ class ODT(QObject):
         p=odf.text.P(stylename=style, text=text)
         self.doc.text.addElement(p)
         
+    def header(self, text, level):
+        h=odf.text.H(outlinelevel=level, stylename="Heading {}".format(level), text=text)
+        self.doc.text.addElement(h)
         
-#    
-#        self.metadata()
-#        self.styles_page()
-#        self.styles_automatic()
-#        self.styles()
-#    
-#        
-#        
-#    def generate(self):
-#        h=odf.text.H(outlinelevel=1, stylename="Heading 1", text=self.tr("Current assets report"))
-#        self.doc.text.addElement(h)
-#        
-#        self.doc.text.addElement(odf.text.P(text=self.tr("Your current balance is {}.").format(self.mem.localcurrency.string(232.233))))
-#        
-#
-#        
-#        self.doc.save(self.filename)
-#    def styles_page(self):  
-#        pagelayout = odf.style.PageLayout(name="PageLayoutVertical")
-#        self.doc.automaticstyles.addElement(pagelayout)
-#        pagelayout.addElement(odf.style.PageLayoutProperties( pagewidth="21cm", pageheight="29.7cm", printorientation="portrait", margintop="2cm",  marginbottom="2cm",  marginright="2cm",  marginleft="2cm"))
-#        
-#        footer=odf.style.Footer()
-#        t=odf.text.PageNumber(text="1", selectpage="current")
-#        p=odf.text.P()
-#        p.addElement(t)
-#        footer.addElement(p)
-#        
-#        mp = odf.style.MasterPage(name="Standard", pagelayoutname=pagelayout)
-#        mp.addElement(footer)
-#        self.doc.masterstyles.addElement(mp)
-#    
-#    def styles_automatic(self):
-#        # Create automatic styles for the column widths.
-#        # We want two different widths, one in inches, the other one in metric.
-#        # ODF Standard section 15.9.1
-#        widthshort = odf.style.Style(name="Wshort", family="table-column")
-#        widthshort.addElement(odf.style.TableColumnProperties(columnwidth="1.7cm"))
-#        self.doc.automaticstyles.addElement(widthshort)
-#        
-#        widthwide = odf.style.Style(name="Wwide", family="table-column")
-#        widthwide.addElement(odf.style.TableColumnProperties(columnwidth="1.5in"))
-#        self.doc.automaticstyles.addElement(widthwide)
-#        
-#        # An automatic style
-#        boldstyle = odf.style.Style(name="Bold", family="text")
-#        boldprop = odf.style.TextProperties(fontweight="bold")
-#        boldstyle.addElement(boldprop)
-#        self.doc.automaticstyles.addElement(boldstyle)
-#        
-#        sPageBreak= odf.style.Style(name="PageBreak", family="paragraph")
-#        sPageBreak.addElement(odf.style.ParagraphProperties(breakbefore="page"))
-#        self.doc.automaticstyles.addElement(sPageBreak)
-#        
-#    
-#
-#    def styles(self):
-#        h1style = odf.style.Style(name="Heading 1", family="paragraph")
-#        h1style.addElement(odf.style.ParagraphProperties(margintop="1cm", marginbottom="0.7cm"))
-#        h1style.addElement(odf.style.TextProperties(fontname="DejaVu Sans", attributes={'fontfamily':"DejaVu Sans",'fontsize':"16pt",'fontweight':"bold" }))
-#        self.doc.styles.addElement(h1style)
-#        
-#        h2style = odf.style.Style(name="Heading 2", family="paragraph")
-#        h2style.addElement(odf.style.ParagraphProperties(margintop="0.8cm", marginbottom="0.5cm"))
-#        h2style.addElement(odf.style.TextProperties(fontname="DejaVu Sans", attributes={'fontfamily':"DejaVu Sans",'fontsize':"16pt",'fontweight':"bold" }))
-#        self.doc.styles.addElement(h2style)
-#        
-#        standard = odf.style.Style(name="Standard", family="paragraph")
-#        standard.addElement(odf.style.ParagraphProperties(margintop="0.2cm", marginbottom="0.2cm"))
-#        standard.addElement(odf.style.TextProperties(fontname="DejaVu Sans", attributes={'fontfamily':"DejaVu Sans",'fontsize':"11pt"}))
-#        self.doc.styles.addElement(standard)
-#        
-#        footer= odf.style.Style(name="Footer", family="paragraph")
-#        footer.addElement(odf.style.ParagraphProperties(textalign="center", ))
-#        footer.addElement(odf.style.TextProperties(fontname="DejaVu Sans", attributes={'fontfamily':"DejaVu Sans","fontstylename":"book",'fontsize':"9pt" }))
-#        self.doc.styles.addElement(footer)
-#        
-#
-#            
-#    def tablacontenido(self):
-#        """LO SAQUE VIENDO UN FODT CON TABLA DE CONTENIDOS"""
-#        content=odf.text.TableOfContent(name="Tabla de contenido")
-#        source=odf.text.TableOfContentSource()
-#        t1=odf.text.TableOfContentEntryTemplate(outlinelevel=1, stylename="Heading 1")
-#        t1.addElement(odf.text.IndexEntryChapter())
-#        t1.addElement(odf.text.IndexEntryText())
-#        t1.addElement(odf.text.IndexEntryPageNumber())
-#        source.addElement(t1)
-#        source.addElement(odf.text.TableOfContentEntryTemplate(outlinelevel=2, stylename="Heading 1"))
-#        source.addElement(odf.text.TableOfContentEntryTemplate(outlinelevel=3, stylename="Heading 1"))
-#        content.addElement(source)
-#        self.doc.text.addElement(content)
-#        
-#    def metadata(self):
-#        self.doc.meta.addElement(odf.dc.Title(text=self.tr("Assets report")))
-#        self.doc.meta.addElement(odf.dc.Description(text=self.tr("This is an automatic generated report from Xulpymoney")))
-#        self.doc.meta.addElement(odf.meta.InitialCreator(text="Xulpymoney-{}".format(version)))
-#        self.doc.meta.addElement(odf.dc.Creator(text=u'đđðæßðđæ€ł'))
-#        
+        
+        
+    def table(self, header, data, sizes, font, number)       :
+        """Headerl text
+        Data: data
+        sizes: arr with column widths in cm
+        size=font size
+        number=name"""  
+    
+        tablesize=sum(sizes)
+        
+        s=odf.style.Style(name="Tabla{}".format(number))
+        s.addElement(odf.style.TableProperties(width="{}cm".format(tablesize), align="center"))
+        self.doc.automaticstyles.addElement(s)
+        
+        #Column sizes
+        for i, size in enumerate(sizes):
+            sc= odf.style.Style(name="Tabla{}.{}".format(number, chr(65+i)), family="table-column")
+            sc.addElement(odf.style.TableColumnProperties(columnwidth="{}cm".format(sizes[i])))
+            self.doc.automaticstyles.addElement(sc)
+        
+        #Cell header style
+        sch=odf.style.Style(name="Tabla{}.HeaderCell".format(number, chr(65), 1), family="table-cell")
+        sch.addElement(odf.style.TableCellProperties(border="0.05pt solid #000000"))
+        self.doc.automaticstyles.addElement(sch)        
+        
+        #Cell normal
+        sch=odf.style.Style(name="Tabla{}.Cell".format(number), family="table-cell")
+        sch.addElement(odf.style.TableCellProperties(border="0.05pt solid #000000"))
+        self.doc.automaticstyles.addElement(sch)
+
+    
+        #Table columns
+        table = odf.table.Table()
+        for i, head in enumerate(header):
+            table.addElement(odf.table.TableColumn(stylename="Tabla{}.{}".format(number, chr(65+i))))  
+            
+        #Header rows
+        headerrow=odf.table.TableHeaderRows()
+        tablerow=odf.table.TableRow()
+        headerrow.addElement(tablerow)
+        for i, head in enumerate(header):
+            p=odf.text.P(stylename="Table Heading", text=head)
+            tablecell=odf.table.TableCell(stylename="Tabla{}.HeaderCell".format(number))
+            tablecell.addElement(p)
+            tablerow.addElement(tablecell)
+        table.addElement(headerrow)
+            
+        #Data rows
+        for row in data:
+            tr = odf.table.TableRow()
+            table.addElement(tr)
+            for col in row:
+                tc = odf.table.TableCell(stylename="Tabla{}.Cell".format(number))
+                tr.addElement(tc)
+                p = odf.text.P(stylename="Table Contents",text=col)
+                tc.addElement(p)
+        
+        self.doc.text.addElement(table)
     def pageBreak(self):    
         p=odf.text.P(stylename="PageBreak")#Is an automatic style
         self.doc.text.addElement(p)
@@ -175,21 +138,32 @@ class AssetsReport(ODT):
         
     def body(self):
         ## About
-        self.simpleParagraph(self.tr("About Xulpymoney"), "Heading 1")
-        self.simpleParagraph(self.tr("About this report"), "Heading 2")
+        self.header(self.tr("About Xulpymoney"), 1)
+        self.header(self.tr("About this report"), 2)
         self.pageBreak()
         ## Assets
-        self.simpleParagraph(self.tr("Assets"), "Heading 1")
+        self.header(self.tr("Assets"), 1)
         self.simpleParagraph(self.tr("The total assets of the user is {}.").format(self.mem.localcurrency.string(self.vTotal)))
         if self.vTotalLastYear!=0:
             moreorless="more"
             if self.vTotal-self.vTotalLastYear<0:
                 moreorless="less"
             self.simpleParagraph(self.tr("It's a {} {} of the total assets at the end of the last year.").format(tpc(100*(self.vTotal-self.vTotalLastYear)/self.vTotalLastYear), moreorless))
+        
+        self.header(self.tr("Assets by bank"), 2)
+        self.table( ["Empresa", "Valor"], [['TEF', 2345], ['SAN', 33456]], [4, 5], 11, 1)       
+        self.simpleParagraph("")
+        
+        self.table( ["Empresa", "Valor"], [['TEF', 2345], ['SAN', 33456]], [2, 5], 11, 2)       
+        
+        self.simpleParagraph("")
+        self.table( ["Empresa", "Valor"], [['TEF', 2345], ['SAN', 33456]], [5, 5], 11, 3)       
+        
+        self.table( ["Empresa", "Valor"], [['TEF', 2345], ['SAN', 33456]], [3, 5], 11, 4)       
         self.pageBreak()
         
         ## Statistics
-        self.simpleParagraph(self.tr("Statistics"), "Heading 1")
+        self.header(self.tr("Statistics"), 1)
         self.pageBreak()
         
         
