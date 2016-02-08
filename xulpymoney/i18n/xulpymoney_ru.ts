9c28d38954fde077c6a6d2fc9eb28e6db0eb9626
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2016-02-08 06:12:46 +0000

    Last now orders

--- xulpymoney/i18n/xulpymoney_ru.ts
+++ xulpymoney/i18n/xulpymoney_ru.ts
@@ -5534,27 +5534,27 @@ Daily Diff: {5}. Investment average age: {6}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/wdgLastCurrent.py" line="118"/>
+        <location filename="../ui/wdgLastCurrent.py" line="122"/>
         <source>Order by</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/wdgLastCurrent.py" line="141"/>
+        <location filename="../ui/wdgLastCurrent.py" line="145"/>
         <source>Shares number: {0}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/wdgLastCurrent.py" line="141"/>
+        <location filename="../ui/wdgLastCurrent.py" line="145"/>
         <source>Purchase price average: {0}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/wdgLastCurrent.py" line="141"/>
+        <location filename="../ui/wdgLastCurrent.py" line="145"/>
         <source>Selling point: {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/wdgLastCurrent.py" line="141"/>
+        <location filename="../ui/wdgLastCurrent.py" line="145"/>
         <source>Selling all shares you get {}</source>
         <translation type="unfinished"></translation>
     </message>
@@ -5584,7 +5584,7 @@ Daily Diff: {5}. Investment average age: {6}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/wdgLastCurrent.py" line="35"/>
+        <location filename="../ui/wdgLastCurrent.py" line="34"/>
         <source>Investment last operation calculator</source>
         <translation type="unfinished"></translation>
     </message>
@@ -5599,7 +5599,7 @@ Daily Diff: {5}. Investment average age: {6}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/wdgLastCurrent.py" line="48"/>
+        <location filename="../ui/wdgLastCurrent.py" line="47"/>
         <source>Divest / Reinvest simulation</source>
         <translation type="unfinished"></translation>
     </message>
--- xulpymoney/ui/wdgLastCurrent.py
+++ xulpymoney/ui/wdgLastCurrent.py
@@ -14,17 +14,16 @@ class wdgLastCurrent(QWidget, Ui_wdgLastCurrent):
         QWidget.__init__(self, parent)
         self.setupUi(self)
         self.mem=mem
-        self.selInvestment=None
         self.tblInvestments.settings(self.mem, "wdgLastCurrent")
         self.spin.setValue(int(self.mem.settingsdb.value("wdgLastCurrent/spin", "-25")))
         self.on_actionSortTPCLast_triggered()
         
     def tblInvestments_reload(self):
-        self.mem.data.investments_active().myqtablewidget_lastCurrent(self.tblInvestments, self.spin.value())
+        self.investments.myqtablewidget_lastCurrent(self.tblInvestments, self.spin.value())
         
     @QtCore.pyqtSlot() 
     def on_actionInvestmentReport_triggered(self):
-        w=frmInvestmentReport(self.mem, self.selInvestment, self)
+        w=frmInvestmentReport(self.mem, self.investments.selected, self)
         w.exec_()
         self.tblInvestments_reload()
         
@@ -34,8 +33,8 @@ class wdgLastCurrent(QWidget, Ui_wdgLastCurrent):
         d.setFixedSize(850, 850)
         d.setWindowTitle(self.tr("Investment last operation calculator"))
         w=wdgCalculator(self.mem)
-        w.cmbProducts.setCurrentIndex(w.cmbProducts.findData(self.selInvestment.product.id))
-        price=self.selInvestment.op_actual.arr[self.selInvestment.op_actual.length()-1].valor_accion*(1+self.spin.value()/Decimal(100))#Is + because -·-
+        w.cmbProducts.setCurrentIndex(w.cmbProducts.findData(self.investments.selected.product.id))
+        price=self.investments.selected.op_actual.last().valor_accion*(1+self.spin.value()/Decimal(100))#Is + because -·-
         w.txtFinalPrice.setText(price)
         lay = QVBoxLayout(d)
         lay.addWidget(w)
@@ -46,8 +45,8 @@ class wdgLastCurrent(QWidget, Ui_wdgLastCurrent):
         d=QDialog()       
         d.resize(self.mem.settings.value("frmInvestmentReport/qdialog_disreinvest", QSize(1024, 768)))
         d.setWindowTitle(self.tr("Divest / Reinvest simulation"))
-        w=wdgDisReinvest(self.mem, self.selInvestment, d)
-        price=self.selInvestment.op_actual.arr[self.selInvestment.op_actual.length()-1].valor_accion*(1+self.spin.value()/Decimal(100))#Is + because -·-        
+        w=wdgDisReinvest(self.mem, self.investments.selected, d)
+        price=self.investments.selected.op_actual.last().valor_accion*(1+self.spin.value()/Decimal(100))#Is + because -·-        
         w.txtValorAccion.setText(price)
         lay = QVBoxLayout(d)
         lay.addWidget(w)
@@ -56,48 +55,53 @@ class wdgLastCurrent(QWidget, Ui_wdgLastCurrent):
                 
     @QtCore.pyqtSlot() 
     def on_actionProduct_triggered(self):
-        w=frmProductReport(self.mem, self.selInvestment.product, self.selInvestment, self)
+        w=frmProductReport(self.mem, self.investments.selected.product, self.investments.selected, self)
         w.exec_()
         self.tblInvestments_reload()
    
     @QtCore.pyqtSlot() 
     def on_actionSortTPCVenta_triggered(self):
-        if self.mem.data.investments_active().order_by_percentage_sellingpoint():
+        self.investments=self.mem.data.investments_active()
+        if self.investments.order_by_percentage_sellingpoint():
             self.tblInvestments_reload()    
         else:
             qmessagebox_error_ordering()     
         
     @QtCore.pyqtSlot() 
     def on_actionSortTPC_triggered(self):
-        if self.mem.data.investments_active().order_by_percentage_invested():
+        self.investments=self.mem.data.investments_active()
+        if self.investments.order_by_percentage_invested():
             self.tblInvestments_reload()    
         else:
             qmessagebox_error_ordering()     
         
     @QtCore.pyqtSlot() 
     def on_actionSortHour_triggered(self):
-        if self.mem.data.investments_active().order_by_datetime_last_operation():
+        self.investments=self.mem.data.investments_active()
+        if self.investments.order_by_datetime_last_operation():
             self.tblInvestments_reload()    
         else:
             qmessagebox_error_ordering()     
         
     @QtCore.pyqtSlot() 
     def on_actionSortName_triggered(self):
-        if self.mem.data.investments_active().order_by_name():
+        self.investments=self.mem.data.investments_active()
+        if self.investments.order_by_name():
             self.tblInvestments_reload()    
         else:
             qmessagebox_error_ordering()     
             
     @QtCore.pyqtSlot() 
     def on_actionSortTPCLast_triggered(self):
-        if self.mem.data.investments_active().order_by_percentage_last_operation():
+        self.investments=self.mem.data.investments_active()
+        if self.investments.order_by_percentage_last_operation():
             self.tblInvestments_reload()    
         else:
             qmessagebox_error_ordering()     
             
 
     def on_tblInvestments_customContextMenuRequested(self,  pos):
-        if self.selInvestment==None:
+        if self.investments.selected==None:
             self.actionInvestmentReport.setEnabled(False)
             self.actionProduct.setEnabled(False)
         else:
@@ -129,19 +133,19 @@ class wdgLastCurrent(QWidget, Ui_wdgLastCurrent):
         self.mem.settingsdb.setValue("wdgLastCurrent/spin", self.spin.value())
 
     def on_tblInvestments_itemSelectionChanged(self):
-        self.selInvestment=None
+        self.investments.selected=None
         for i in self.tblInvestments.selectedItems():#itera por cada item no row.
-            self.selInvestment=self.mem.data.investments_active().arr[i.row()]
+            self.investments.selected=self.investments.arr[i.row()]
 
     @QtCore.pyqtSlot(int, int) 
     def on_tblInvestments_cellDoubleClicked(self, row, column):
         if column==8:#TPC Venta
             m=QMessageBox()
             m.setIcon(QMessageBox.Information)
-            m.setText(self.tr("Shares number: {0}").format(self.selInvestment.acciones())+"\n"+
-                    self.tr("Purchase price average: {0}").format(self.selInvestment.product.currency.string(self.selInvestment.op_actual.valor_medio_compra()))+"\n"+
-                    self.tr("Selling point: {}").format(self.selInvestment.product.currency.string(self.selInvestment.venta))+"\n"+
-                    self.tr("Selling all shares you get {}").format(self.selInvestment.product.currency.string(self.selInvestment.op_actual.pendiente(Quote(self.mem).init__create(self.selInvestment.product, self.mem.localzone.now(),  self.selInvestment.venta))))
+            m.setText(self.tr("Shares number: {0}").format(self.investments.selected.acciones())+"\n"+
+                    self.tr("Purchase price average: {0}").format(self.investments.selected.product.currency.string(self.investments.selected.op_actual.valor_medio_compra()))+"\n"+
+                    self.tr("Selling point: {}").format(self.investments.selected.product.currency.string(self.investments.selected.venta))+"\n"+
+                    self.tr("Selling all shares you get {}").format(self.investments.selected.product.currency.string(self.investments.selected.op_actual.pendiente(Quote(self.mem).init__create(self.investments.selected.product, self.mem.localzone.now(),  self.investments.selected.venta))))
             )
             m.exec_()     
         else:
