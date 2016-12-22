9d58a1be155d17185b9940441b88ca9c65ee29a7
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2016-12-22 07:55:10 +0000

    Trying to improve morningstar

--- xulpymoney/sources/source_morningstar.py
+++ xulpymoney/sources/source_morningstar.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
 sys.path.append("/usr/lib/xulpymoney")
-from libxulpymoney import *
-from libsources import *
+from libxulpymoney import MemXulpymoney
+from libsources import WorkerMorningstar
 
-mem=MemProducts()
+mem=MemXulpymoney()
 mem.init__script('Morningstar update')
-
-w=WorkerMorningstar(mem, 1, "select * from products where type=2 and char_length(isin)>0  and obsolete=false")
+w=WorkerMorningstar(mem, 1)
+w.setSQL("select * from products where type=2 and char_length(isin)>0  and obsolete=false")
 w.run()
-
-mem.disconnect(mem.con)
+mem.con.disconnect()
