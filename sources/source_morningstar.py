69b6f1f0e1d63afa8d1af0fc96f4780deb8060f0
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2017-01-08 13:05:54 +0000

    Now concepts in frmAccountReport

--- sources/source_morningstar.py
+++ sources/source_morningstar.py
@@ -5,8 +5,14 @@ from libxulpymoney import MemXulpymoney
 from libsources import WorkerMorningstar
 
 mem=MemXulpymoney()
-mem.init__script('Morningstar update')
+args=mem.init__script('Morningstar update',  type=1)
+
+
 w=WorkerMorningstar(mem, 1)
-w.setSQL("select * from products where type=2 and char_length(isin)>0  and obsolete=false")
-w.run()
+w.setSQL(useronly=False)#"select * from products where type=2 and char_length(isin)>0  and obsolete=false")
+
+if args.tickers==True:
+    w.regenerate_tickers()
+else:
+    w.run()
 mem.con.disconnect()
