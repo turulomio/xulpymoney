c0ba2764c2db7a79a8ff6e40786bcfa3dff38831
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2017-10-29 15:02:07 +0000

    Improving QTcharts historicla product

--- xulpymoney/ui/wdgProductHistoricalChart.py
+++ xulpymoney/ui/wdgProductHistoricalChart.py
@@ -5,6 +5,7 @@ from Ui_wdgProductHistoricalChart import Ui_wdgProductHistoricalChart
 import datetime
 from canvaschart import VCCandlestick,   VCTemporalSeries
 from libxulpymoney import day_end_from_date,  OHCLDuration
+
 class wdgProductHistoricalChart(QWidget, Ui_wdgProductHistoricalChart):
     def __init__(self,  parent=None):
         QWidget.__init__(self, parent)
@@ -39,39 +40,52 @@ class wdgProductHistoricalChart(QWidget, Ui_wdgProductHistoricalChart):
         if self.cmbChartType.currentIndex()==0:#Lines
             self.view=VCTemporalSeries()
             self.verticalLayout.addWidget(self.view)
-            ls=self.view.appendSeries(self.product.name.upper(), self.product.currency)
+            ls=self.view.appendSeries(self.product.name.upper(), self.product.currency)#Line seies
             for ohcl in self.setohcl.arr:
                 if ohcl.datetime()>=selected_datetime:
                     self.view.appendData(ls, ohcl.datetime(), ohcl.close)
                     
             if self.chkSMA50.isChecked() and self.setohcl.length()>50:
-                sma50=self.view.appendSeries(self.tr("SMA50"),  self.product.currency)
+                sma50=self.view.appendSeries(self.tr("SMA50"),  self.product.currency)#SMA50 line series
                 for dt, value in self.setohcl.sma(50):
                     if selected_datetime<=dt:
                         self.view.appendData(sma50, dt, value)
-            if self.chkSMA200.isChecked() and self.setohcl.length()>200:
+                        
+            if self.chkSMA200.isChecked() and self.setohcl.length()>200:#SMA200 line sries
                 sma200=self.view.appendSeries(self.tr("SMA200"),  self.product.currency)
                 for dt, value in self.setohcl.sma(200):
                     if selected_datetime<=dt:
                         self.view.appendData(sma200, dt, value)
-
+        #################################
         elif self.cmbChartType.currentIndex()==1:#Candles
             self.view=VCCandlestick()
             self.verticalLayout.addWidget(self.view)
-            self.view.appendSeries(self.product)
-            self.view.setFrom(selected_datetime,  self.cmbOHCLDuration.itemData(self.cmbOHCLDuration.currentIndex()))
             
-            if self.chkSMA50.isChecked()and self.setohcl.length()>50:
+            candle=self.view.appendCandlestickSeries(self.product.name, self.product.currency)#Candle series
+            for ohcl in self.setohcl.arr:
+                if ohcl.datetime()>=selected_datetime:
+                    self.view.appendCandlestickSeriesData(candle, ohcl)
+            self.view.setOHCLDuration(self.cmbOHCLDuration.itemData(self.cmbOHCLDuration.currentIndex()))
+            
+            if self.chkSMA50.isChecked() and self.setohcl.length()>50:#SMA50 line series
                 sma50=self.view.appendTemporalSeries(self.tr("SMA50"),  self.product.currency)
-                print(self.setohcl, self.cmbOHCLDuration.itemData(self.cmbOHCLDuration.currentIndex()))
                 for dt, value in self.setohcl.sma(50):
                     self.view.appendTemporalSeriesData(sma50, dt, value)
                     
-            if self.chkSMA200.isChecked() and self.setohcl.length()>200:
+            if self.chkSMA200.isChecked() and self.setohcl.length()>200:#SMA200 line series
                 sma200=self.view.appendTemporalSeries(self.tr("SMA200"),  self.product.currency)
                 for dt, value in self.setohcl.sma(200):
                     self.view.appendTemporalSeriesData(sma200, dt, value)
-                
+                    
+        #INVESTMENT
+        if self.investment!=None:
+            buy=self.view.appendScatterSeries(self.tr("Buy operations"), self.product.currency)
+            sell=self.view.appendScatterSeries(self.tr("Sell operations"), self.product.currency)
+            for op in self.investment.op.arr:
+                if op.tipooperacion.id in (4, ) and op.datetime>selected_datetime:
+                    self.view.appendScatterSeriesData(buy, op.datetime, op.valor_accion)
+                if op.tipooperacion.id in (5, ) and op.datetime>selected_datetime:
+                    self.view.appendScatterSeriesData(sell, op.datetime, op.valor_accion)
         self.view.display()
             
     @pyqtSlot(int)      
