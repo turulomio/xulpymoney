56c8a5a4f184a784ebccdaf673a48ccf7b67983c
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-08-19 18:14:18 +0000

    Volviendo a pelo a 2681

--- libxulpymoneyversion.py
+++ libxulpymoneyversion.py
@@ -1,15 +1,16 @@
-## @package xulpymoney
+## @package libxulpymoneyversion
 ## @brief Package with xulpymoney version information
 import datetime
 
-__version__="0.0.1"
-__versiondate__=datetime.date(2018,8,19)
 version="20180518"
 
 def version_windows():
 #    lastpoint="0"
 #    if version.find("+")!=-1:
 #        lastpoint="1"
-    vd=__versiondate__
+    vd=version_date()
     return "{}.{}.{}".format(vd.year, vd.month, vd.day)
     
+def version_date():
+    versio=version.replace("+","")
+    return datetime.date(int(versio[:-4]),  int(versio[4:-2]),  int(versio[6:]))
