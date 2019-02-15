17361be6f99a243a17089fd06699725ab329186d
turulomio <turulomio@yahoo.es>
2019-02-15 08:40:26 +0100

    Seems to work

--- xulpymoney/iocalculations.py
+++ xulpymoney/iocalculations.py
@@ -1,35 +1,46 @@
-#op=[1,-1,-2,2,3,4,-7,-3,-4,7] #Exacto
+op=[1,-1,-2,2,3,4,-7,-3,-4,7] #Exacto
 #op=[1,-2,2,-3,4] #Cambio signo
-op=[1,1,-3] #Cambio signo generando 2 hst
+#op=[1,1,-3, 1] #Cambio signo generando 2 hst
 
 cur=[]
 hst=[]
 
-for position, io in enumerate(op):
-    if sum(cur)>=0 and io>0:
-        cur.append(io)
-    elif sum(cur)<=0 and io<0:
-        cur.append(io)
-    elif sum(cur)+io==0:
-        cur=[]
-        hst.append(io)
-    else:
-        rest=None
-        while rest!=0 or len(cur)==0:
-            if rest==None:
-                rest=0
-            first=cur[0]
-            cur.pop(0)
-            if (first>0 and first+rest<0) or (first<0 and first+rest>0): #
-                hst.append(-first)
-                rest=rest+first
-                cur.insert(0, rest)
-            else: # 
-                hst.append(first)
-                rest=rest+first
-                break
 
-            print("REST", rest, op, cur, hst)
-    print(io, op, cur, hst)
-    
-    
+def same_sign(number1, number2):
+    if (is_positive(number1)==True and is_positive(number2)==True) or (is_positive(number1)==False and is_positive(number2)==False):
+        return True
+    return False
+
+def is_positive(number):
+    if number>=0:
+        return True
+    return False
+
+
+print("Processing {}".format(op))
+
+for position, io in enumerate(op):
+    if same_sign(sum(cur), io)==True:
+        cur.append(io)
+    elif same_sign(sum(cur), io)==False:
+        rest=io
+        while(rest!=0 and len(cur)!=0):
+            first=cur[0]
+            if same_sign(first, rest)==False:
+                if abs(first)>abs(rest):
+                    hst.append(-rest)
+                    cur.pop(0)
+                    if rest+first!=0:
+                        cur.insert(0, rest+first)
+                    rest=0
+                else: #Mayor el resto
+                    hst.append(-first)
+                    rest=rest+first
+                    cur.pop(0)
+#                print("     REST", rest, "Current", cur, "Historical", hst)
+            else:
+                cur.insert(0, rest)
+                break
+        if rest!=0:
+            cur.insert(0, rest)
+    print("IO", io, "Current", cur, "Historical", hst)
