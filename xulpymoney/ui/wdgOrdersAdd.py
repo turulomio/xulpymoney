5186c67f4c82a94e369328af15a69a0145342c33
caworky <timetome1973@hotmail.es>
2019-08-06 12:44:40 +0200

    Improved order edition

--- xulpymoney/ui/wdgOrdersAdd.py
+++ xulpymoney/ui/wdgOrdersAdd.py
@@ -17,25 +17,25 @@ class wdgOrdersAdd(QWidget, Ui_wdgOrdersAdd):
             self.deDate.setDate(date.today())
             self.lbl.setText("Add new order")
             self.deExpiration.setDate(date.today())
+            product=self.mem.data.products.find_by_id(int(self.mem.settings.value("wdgCalculator/product", -9999)))
+            self.mem.data.investments.ProductManager_with_investments_distinct_products().qcombobox_not_obsolete(self.cmbProducts, product)
         else:
             self.lbl.setText("Edit order")
             self.deDate.setDate(self.order.date)
             self.deExpiration.setDate(self.order.expiration)
             self.txtShares.setText(self.order.shares)
-            self.txtAmount.setText(self.order.amount)
             self.txtPrice.setText(self.order.price)
-        
-        product=self.mem.data.products.find_by_id(int(self.mem.settings.value("wdgCalculator/product", -9999)))
-        self.mem.data.investments.ProductManager_with_investments_distinct_products().qcombobox_not_obsolete(self.cmbProducts, product)
-#        if self.investment==None:
-#            self.mem.data.investments.qcombobox(self.cmbInvestments, 2, selected=None, obsolete_product=False, investments_active=None,  accounts_active=True)
-#        else:
-#            self.mem.data.investments.qcombobox(self.cmbInvestments, 2, selected=investment, obsolete_product=False, investments_active=None,  accounts_active=True)
+            if investment==None:
+                product=self.mem.data.products.find_by_id(int(self.mem.settings.value("wdgCalculator/product", -9999)))
+                self.mem.data.investments.ProductManager_with_investments_distinct_products().qcombobox_not_obsolete(self.cmbProducts, product)
+            else:
+                self.mem.data.investments.ProductManager_with_investments_distinct_products().qcombobox_not_obsolete(self.cmbProducts, investment.product)
+                self.cmbInvestments.setCurrentIndex(self.cmbInvestments.findData(investment.id))
 
     @pyqtSlot()
     def on_buttonbox_accepted(self):
         self.date=self.deDate.date()
-        if not (self.txtAmount.isValid() and self.txtPrice.isValid() and self.txtShares.isValid()):
+        if not ( self.txtPrice.isValid() and self.txtShares.isValid()):
             qmessagebox(self.tr("Incorrect data. Try again."))
             return
         investment=self.mem.data.investments.find_by_id(self.cmbInvestments.itemData(self.cmbInvestments.currentIndex()))
@@ -45,16 +45,16 @@ class wdgOrdersAdd(QWidget, Ui_wdgOrdersAdd):
         if self.deExpiration.date()<self.deDate.date():
             qmessagebox(self.tr("Expiration date can't be less than order date."))
             return
-            
+
         if self.order==None:
             self.order=Order(self.mem)
         self.order.date=self.deDate.date().toPyDate()
         self.order.expiration=self.deExpiration.date().toPyDate()
         self.order.shares=self.txtShares.decimal()
-        self.order.amount=self.txtAmount.decimal()
+        self.order.amount=round(self.txtPrice.decimal()*self.txtShares.decimal(), 2)
         self.order.price=self.txtPrice.decimal()
         self.order.investment=investment
-        
+
         self.order.save()
         self.mem.con.commit()
         self.order.qmessagebox_reminder()
@@ -63,32 +63,35 @@ class wdgOrdersAdd(QWidget, Ui_wdgOrdersAdd):
     @pyqtSlot()
     def on_buttonbox_rejected(self):
         self.parent.reject()
-            
+
     @pyqtSlot(int)  
     def on_cmbProducts_currentIndexChanged(self, index):
-        """To invoke this function you must call self.cmbProducts.setCurrentIndex()"""
         product=self.mem.data.products.find_by_id(self.cmbProducts.itemData(index))
         if product!=None:
             #Fills self.cmbInvestments with all product investments or with zero shares product investments
             if self.chkWithoutShares.checkState()==Qt.Checked:
-                self.investments=self.mem.data.investments.InvestmentManager_with_investments_with_the_same_product_with_zero_shares(product)
+                investments=self.mem.data.investments.InvestmentManager_with_investments_with_the_same_product_with_zero_shares(product)
             else:
-                self.investments=self.mem.data.investments.InvestmentManager_with_investments_with_the_same_product(product)
-            self.investments.qcombobox(self.cmbInvestments, tipo=3, selected=None, obsolete_product=False, investments_active=None, accounts_active=None)
- 
+                investments=self.mem.data.investments.InvestmentManager_with_investments_with_the_same_product(product)
+            investments.qcombobox(self.cmbInvestments, tipo=3, selected=None, obsolete_product=False, investments_active=None, accounts_active=None)
             self.mem.settings.setValue("wdgCalculator/product", product.id)
-    
+
     @pyqtSlot(int)  
     def on_cmbInvestments_currentIndexChanged(self, index):
-        """To invoke this function you must call self.cmbInvestments.setCurrentIndex()"""
         if index>=0:#Only enabled if some investment is selected
             self.buttonbox.button(QDialogButtonBox.Ok).setEnabled(True)
         else:
             self.buttonbox.button(QDialogButtonBox.Cancel).setEnabled(True)
-        self.investments.selected=self.mem.data.investments.find_by_id(self.cmbInvestments.itemData(index))
-
 
     ## This check filters investments showing ones with 0 currrent shares
     def on_chkWithoutShares_stateChanged(self, state):
         self.on_cmbProducts_currentIndexChanged(self.cmbProducts.currentIndex())
-        
+
+    def on_txtShares_textChanged(self):
+        if self.txtShares.isValid() and self.txtPrice.isValid():
+            self.txtAmount.setText(round(self.txtShares.decimal()*self.txtPrice.decimal(), 2))
+        else:
+            self.txtAmount.setText("")
+
+    def on_txtPrice_textChanged(self):
+        self.on_txtShares_textChanged()
