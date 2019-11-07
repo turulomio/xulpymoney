429698ab5e09fa75b7883ab0430aa96804241aca
turulomio <turulomio@yahoo.es>
2019-11-07 06:45:17 +0100

    Added product

---
+++ xulpymoney/sql/201911061105.sql
@@ -0,0 +1,4 @@
+select;
+-- Adding ITALIAN MIB Y LEB
+INSERT INTO public.products (name        , isin, currency, type, agrupations, id   , web , address, phone, mail, percentage, pci, leveraged, stockmarkets_id, comment, obsolete, tickers                      , high_low, decimals) VALUES 
+                            ('FUTURO MIB', NULL, 'EUR'   , 13  , ''         , 81746, NULL, NULL   , NULL , NULL, 100       , 'p', 5        , 6              , NULL   , false   , '{NULL,NULL,NULL,NULL,IFSc1}', true    , 2);
