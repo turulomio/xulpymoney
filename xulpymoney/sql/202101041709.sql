251020b7ee70cc011d11f5db9af217ccb7a47cf2
turulomio <turulomio@yahoo.es>
2021-01-04 17:11:18 +0100

    Added additional 6-10 in strategy table

---
+++ xulpymoney/sql/202101041709.sql
@@ -0,0 +1,6 @@
+ALTER TABLE public.strategies ADD COLUMN additional6 integer;
+ALTER TABLE public.strategies ADD COLUMN additional7 integer;
+ALTER TABLE public.strategies ADD COLUMN additional8 integer;
+ALTER TABLE public.strategies ADD COLUMN additional9 integer;
+ALTER TABLE public.strategies ADD COLUMN additional10 integer;
+
--- xulpymoney/version.py
+++ xulpymoney/version.py
@@ -2,5 +2,5 @@
 from datetime import datetime
 
 __version__ ='0.14.9999'
-__versiondatetime__=datetime(2020, 11, 15, 17, 35)
+__versiondatetime__=datetime(2021, 1, 4, 17, 9)
 __versiondate__=__versiondatetime__.date()
