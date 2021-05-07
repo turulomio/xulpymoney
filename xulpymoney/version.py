510bbf339eb2dac41f1a40f43d26e7b8953ece4b
turulomio <turulomio@yahoo.es>
2021-05-07 06:02:06 +0200

    Updated function investment_operations

--- xulpymoney/version.py
+++ xulpymoney/version.py
@@ -2,5 +2,5 @@
 from datetime import datetime
 
 __version__ ='0.14.9999'
-__versiondatetime__=datetime(2021, 4, 26, 7, 48)
+__versiondatetime__=datetime(2021, 5, 7, 6, 0)
 __versiondate__=__versiondatetime__.date()
