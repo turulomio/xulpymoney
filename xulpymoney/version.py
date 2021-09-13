942bd3e8eb6a7f0403db1600dce642c37cc4ca56
turulomio <turulomio@yahoo.es>
2021-09-13 08:14:42 +0200

    Added tablename to investment_operations function

--- xulpymoney/version.py
+++ xulpymoney/version.py
@@ -2,5 +2,5 @@
 from datetime import datetime
 
 __version__ ='0.14.9999'
-__versiondatetime__=datetime(2021, 9, 5, 19, 10)
+__versiondatetime__=datetime(2021, 9, 12, 8, 59)
 __versiondate__=__versiondatetime__.date()
